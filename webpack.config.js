c20e92d957b4acf133b2867533e408ee5e825dc5
Santhosh K <96395224+Santhosh-BE@users.noreply.github.com>
2025-01-25 14:50:29 +0530

    ,,

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
-const path = require('path');
+import { resolve as _resolve } from 'path';
 
-module.exports = {
-  entry: './client.js',
-  mode: process.env.NODE_ENV || 'development',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-    ],
-  },
+export const entry = './client.js';
+export const mode = 'development';
+export const output = {
+  // eslint-disable-next-line no-undef
+  path: _resolve(__dirname, 'build'),
+  filename: 'bundle.js',
+  publicPath: '/',
+};
+export const resolve = {
+  extensions: ['.js', '.jsx', '.json'],
+};
+export const module = {
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: 'babel-loader',
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    },
+  ],
 };
